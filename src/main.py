30b8fcc4796c5b83c0313476b16e953fa6ff5dea
Huandong Chang <changhua@grinnell.edu>
2020-08-04 10:14:45 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,19 +6,18 @@ from initialization import *
 from RegularInitialization import *
 
 ####################################################################################################
-#Parameter that require manual inputs
+#Hand-tune Parameters
 ####################################################################################################
-keyNumber = 4 #number of keys, assume they are 'a','b','c','s'.
-
+keyNumber = 4  #number of keys, including space bar
 key_lst = ['a','b','c','s']
-prob = [0.1,0.1,0.4,0.4] # Probability of each individual keys
-w = 110000 # number of triples we want to generate
+prob = [0.1,0.1,0.4,0.4] # Probability of each individual keys (3D Typing Model)
+w = 110 # number of triples we want to generate
 nodes = 15000 # number of nodes we want to generate
-alpha = 0.9 #blue factor
-beta = 0.95 #additional factor
+alpha = 0.9 #factor for entries with two same letters
+beta = 0.95 #additional factor for entries with three different letters
 
 ####################################################################################################
-#End of manual inputs
+#End of Hand-tune Parameters
 ####################################################################################################
 
 
@@ -54,12 +53,13 @@ def initialization(w):
     mplst.clear()
     melst.clear()
 
-    #Tensor
+    #Initialize the tensor
     tensor_initialization(keyNumber,key_lst,prob,tensor,pt,plst,elst)
 
-    #matrix
+    #Initialize the matrix
     matrix_initialization(keyNumber,key_lst,prob,matrix,pm,mplst,melst)
 
+    #multiply alpha and beta parameters to our
     imbalance(pt,keyNumber,alpha,beta,prob,plst)
 
     imbalanceMatrix(pm,keyNumber,beta,prob,mplst)
@@ -75,8 +75,15 @@ initialization(w)
 
 
 ###########################Initialization for 2D Typing Model####################################
+# The following code is used only if we need to use original typing model to add edges
+# parameters:
+# pairs: the number of pairs we want to generate
+# keyNumber: the number of keys (including space abr)
+# key_lst: the list of keys
+# prob: probability list for each key
 def initialization2D(pairs,keyNumber,key_lst,prob):
 
+    #we need to clear all the global variables since the parameters are different
     tensor.clear()
     matrix.clear()
     pt.clear()
@@ -87,53 +94,18 @@ def initialization2D(pairs,keyNumber,key_lst,prob):
     melst.clear()
 
 
-    #matrix
+    #We only need to initialize the matrix since tensor is not used in the  original typing model
     matrix_initialization(keyNumber,key_lst,prob,matrix,pm,mplst,melst)
-
-
     imbalanceMatrix(pm,keyNumber,beta,prob,mplst)
 
+    #add edges to graph G
     new_graph=generate_graph2D(G,pairs,elst,plst,melst,mplst,key_lst,prob)
-initialization2D(130000,keyNumber,key_lst,[0.25,0.25,0.1,0.4])
+# initialization2D(130000,keyNumber,key_lst,[0.25,0.25,0.1,0.4])
 ###########################Initialization for 2D Typing Model End####################################
-print("Nodes: "+str(G.order()))
-print("Edges: "+str(G.size()))
-print('Average clustering coefficient: '+str(nx.average_clustering(G)))
-print('Global clustering coefficient: '+str(nx.transitivity(G)))
-print('Size of the LCC: '+str(len(max(nx.connected_components(G), key=len))))
-
-###########################Erdos Renyi Model#########################################################
-ConMat_Nodes=23133
-ConMat_Edges=93497
-
-def Erdos_Renyi(nodes,edges):
-    ER = nx.gnm_random_graph(nodes, edges)
-    G.add_edges_from(ER.edges)
-    G.add_nodes_from(ER.nodes)
-
-# Erdos_Renyi(ConMat_Nodes-G.order(),ConMat_Edges-G.size())
-###########################Erdos Renyi Model End######################################################
 
 
-########################Initialization End##############################################################
-
-
-
-
-
-####################################################################################################
-#Execute
-####################################################################################################
-
-##########################Draw Graph#################################
-# nx.draw(G, with_labels=False,node_size=5, edge_color='grey')
-# nx.draw_spring(G, with_labels=False,node_size=40)
-# nx.draw_planar(G, with_labels=True)
-# nx.draw_random(G, with_labels=False,node_size=40)
-# plt.show()
-###########################Draw Graph End#################################
-
-
-####################################################################################################
-#Execute End
-####################################################################################################
+print("Nodes: "+str(G.order())) #number of nodes
+print("Edges: "+str(G.size()))  #number of edges
+print('Average clustering coefficient: '+str(nx.average_clustering(G))) #Average clustering coefficient
+print('Global clustering coefficient: '+str(nx.transitivity(G))) #Global clustering coefficient
+print('Size of the LCC: '+str(len(max(nx.connected_components(G), key=len)))) #Largest Connected Component
